68ca4531ababd6d8ae0e4434176c4d7365b69230
AndriyDubeniuk <113316073+AndriyDubeniuk@users.noreply.github.com>
2022-09-20 23:53:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
-
 $(document).ready(function() {
     DisplayData();
     let buttons = $("#allbuttuons").clone();
     $("#clonebuttons").append(buttons);
     buttons.find(".selectActionUp").removeClass("selectActionUp").addClass("selectActionDown");
     buttons.find(".buttonOkUp").removeClass("buttonOkUp").addClass("buttonOkDown");
+
+    // from array in select
+    // let role = ["User", "Admin"];
+    // $.each(role, function (index, value) {
+    //     $("#role").append($("<option>", {
+    //         value: value,
+    //         text: value
+    //     }));
+    // });
+
+    $("#user_form_modal").on("keyup", function() {
+        if ($("#first_name").val() != "") {
+            $("#error").text('');
+            if ($("#last_name").val() != "") {
+                $("#error").text('');
+                $("#role").on("change", function() {
+                    if ($("#role").val() != "") {
+                        $("#error").text('');
+                    }
+                });
+            }
+        }
+    });
+
+    $("#displayDataTable").bind("DOMSubtreeModified", function() {
+        if ($("#all-items").prop("checked")==true) {
+            $(".check-action").prop("checked", true);
+        }
+    });
 
 });
 
@@ -45,14 +73,13 @@ $(document).on("click", ".buttonOkUp", function() {
         AlertWindow("Alert!","No users selected! Please select a user!");
     } else {
         if($(".selectActionUp").val() == "SetActive") {
-            SetActivity(arr_id, "active");
+            SetActivity(arr_id, 1);
         } else if($(".selectActionUp").val() == "SetNotActive") {
-            SetActivity(arr_id, "not-active");
+            SetActivity(arr_id, 0);
         } else if ($(".selectActionUp").val() == "Delete") {
             DeleteConfirm(arr_id);
         } else {
             AlertWindow("Alert!","No action selected! Please select an action!");
-
         }
     }
 });
@@ -68,9 +95,9 @@ $(document).on("click", ".buttonOkDown", function() {
         AlertWindow("Alert!","No users selected! Please select a user!");
     } else {
         if($(".selectActionDown").val() == "SetActive") {
-            SetActivity(arr_id, "active");
+            SetActivity(arr_id, 1);
         } else if($(".selectActionDown").val() == "SetNotActive") {
-            SetActivity(arr_id, "not-active");
+            SetActivity(arr_id, 0);
         } else if ($(".selectActionDown").val() == "Delete") {
             DeleteConfirm(arr_id);       
         } else {
@@ -109,7 +136,7 @@ function DisplayData() {
         data: {
             displaySend:displayData
         },
-        success:function(data){
+        success:function(data, status){
             $(".table").append(data);
         }
     });
@@ -119,7 +146,9 @@ function AddUser(){
     let first_nameAdd = $('#first_name').val();
     let last_nameAdd = $('#last_name').val();
     let roleAdd = $('#role').val();
-    let statusAdd = $('#switch').prop("checked") ? "active" : "not-active";
+    let statusAdd = $('#switch').prop("checked") ? 1 : 0;
+    let statusUser = "";
+    let roles = [];
 
     $.ajax({
         url:"insert.php",
@@ -130,9 +159,11 @@ function AddUser(){
             roleSend:roleAdd,
             statusSend:statusAdd
         },
-        success:function(data) {
+        success:function(data, status) {
             let adddata=JSON.parse(data);
             if(adddata.status == "true") {
+                if (statusAdd == 1) { statusUser = "active"; } 
+                else if (statusAdd == 0) { statusUser = "not-active"; }
                 $('#user_form_modal').modal('hide');
                 let table = "";
                 table += `<tr id="row_${adddata.user.id}">
@@ -142,9 +173,9 @@ function AddUser(){
                         <label class="custom-control-label" for="item_${adddata.user.id}"></label>
                     </div>
                 </td>
-                <td class="text-nowrap align-middle">${first_nameAdd} ${last_nameAdd}</td>
-                <td class="text-nowrap align-middle"><span>${roleAdd}</span></td>
-                <td class="text-center align-middle"><i class="fa fa-circle ${statusAdd}-circle" id="status" value=${statusAdd}></i></td>
+                <td class="text-nowrap align-middle first-last-name">${first_nameAdd} ${last_nameAdd}</td>
+                <td class="text-nowrap align-middle role-name"><span>${roleAdd}</span></td>
+                <td class="text-center align-middle status-name"><i class="fa fa-circle ${statusUser}-circle" id="status" value=${statusAdd}></i></td>
                 <td class="text-center align-middle">
                     <div class="btn-group align-top">
                         <button class="btn btn-sm btn-outline-secondary badge" type="button" data-toggle="modal" data-target="#user_form_modal" onclick="GetDetails(${adddata.user.id})">Edit</button>
@@ -169,10 +200,9 @@ function DeleteUser(arr_id) {
         data: {
             arr_idSend:arr_id
         },
-        success:function() {
+        success:function(data, status) {
             if (arr_id.length == $(".check-action").length) {
                 $("#all-items").removeAttr("checked"); 
-                console.log("daddadadadadad");
             }    
             for(let i=0; i<arr_id.length; i++) {
                 $("#row_"+arr_id[i]).remove();
@@ -189,8 +219,10 @@ function SetActivity(activityIdArr,statusUser) {
             activitySend:activityIdArr,
             statusUserSend:statusUser
         },
-        success:function() {
-            let activity = `<i class="fa fa-circle ${statusUser}-circle" id="status" value="${statusUser}"></i>`;
+        success:function(data, status) {
+            if (statusUser == 1) { statusUserSet = "active"; } 
+            else if (statusUser == 0) { statusUserSet = "not-active"; }
+            let activity = `<i class="fa fa-circle ${statusUserSet}-circle" id="status" value="${statusUser}"></i>`;
             for(let i=0; i<activityIdArr.length; i++) {
                 $("#row_"+activityIdArr[i]).find("i.fa-circle").replaceWith(activity);
             }
@@ -204,12 +236,12 @@ function GetDetails(updateId) {
     $('#hiddendata').val(updateId);
     $.post("get.php",
         { updateId:updateId }, 
-        function(data) {
+        function(data, status) {
             let userid=JSON.parse(data);
             $('#first_name').val(userid.user.firstname);
             $('#last_name').val(userid.user.lastname);
             $('#role').val(userid.user.role);
-            userid.user.status == "active" ? $('#switch').prop("checked", true) : $('#switch').prop("checked", false);
+            userid.user.status == 1 ? $('#switch').prop("checked", true) : $('#switch').prop("checked", false);
             $("#error").text('');
         });
 }
@@ -218,8 +250,9 @@ function UpdateDetails() {
     let update_first_name = $('#first_name').val();
     let update_last_name = $('#last_name').val();
     let update_role = $('#role').val();
-    let update_status = $('#switch').prop("checked") ? "active" : "not-active";
+    let update_status = $('#switch').prop("checked") ? 1 : 0;
     let hiddendata=$('#hiddendata').val();
+    let statusUser = "";
 
     $.post("update.php", {
         update_first_name:update_first_name,
@@ -227,30 +260,19 @@ function UpdateDetails() {
         update_role:update_role,
         update_status:update_status,
         hiddendata:hiddendata
-    }, function(data) {
+    }, function(data, status) {
         let updatedata=JSON.parse(data);
         if(updatedata.status == "true") {
-            let table = "";
-            table += `<tr id="row_${hiddendata}">
-            <td class="align-middle">
-                <div class="custom-control custom-control-inline custom-checkbox custom-control-naeless m-0 align-top">
-                    <input type="checkbox" class="custom-control-input check-action" id="item_${hiddendata}" value="${hiddendata}">
-                    <label class="custom-control-label" for="item_${hiddendata}"></label>
-                </div>
-            </td>
-            <td class="text-nowrap align-middle">${update_first_name} ${update_last_name}</td>
-            <td class="text-nowrap align-middle"><span>${update_role}</span></td>
-            <td class="text-center align-middle"><i class="fa fa-circle ${update_status}-circle" id="status" value=${update_status}></i></td>
-            <td class="text-center align-middle">
-                <div class="btn-group align-top">
-                    <button class="btn btn-sm btn-outline-secondary badge" type="button" data-toggle="modal" data-target="#user_form_modal" onclick="GetDetails(${hiddendata})">Edit</button>
-                    <button class="btn btn-sm btn-outline-secondary badge delete-user" data-toggle="modal" data-target="#modal-confirm" value=${hiddendata} type="button"><i
-                    class="fa fa-trash"></i></button>
-                </div>
-            </td>
-        </tr>`;
-        $("#row_"+hiddendata).replaceWith(table);
+            if (update_status == 1) { statusUser = "active"; } 
+            else if (update_status == 0) { statusUser = "not-active"; }
+            let tdname = `<td class="text-nowrap align-middle first-last-name">${update_first_name} ${update_last_name}</td>`;
+            let tdrole = `<td class="text-nowrap align-middle role-name"><span>${update_role}</span></td>`;
+            let tdstatus = `<td class="text-center align-middle status-name"><i class="fa fa-circle ${statusUser}-circle" id="status" value=${update_status}></i></td>`;
+            $("#row_"+hiddendata).find("td.first-last-name").replaceWith(tdname);
+            $("#row_"+hiddendata).find("td.role-name").replaceWith(tdrole);
+            $("#row_"+hiddendata).find("td.status-name").replaceWith(tdstatus);
         }
         $('#user_form_modal').modal('hide');
+
     });
 }
